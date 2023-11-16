91729d79d53538f52105d644b05e6795f91371bd
Rob <61951374+Leo4j@users.noreply.github.com>
2023-11-16 11:57:39 +0000

    Update Validate-Credentials.ps1

--- Validate-Credentials.ps1
+++ Validate-Credentials.ps1
@@ -3,6 +3,6 @@ Add-Type -AssemblyName System.DirectoryServices.AccountManagement
 function Validate-Credentials($username, $password){
 
   $context = New-Object System.DirectoryServices.AccountManagement.PrincipalContext("Domain")
-  if($context.ValidateCredentials($username, $password)){"[+] Credentials Validation: Success"}
+  if($context.ValidateCredentials($username, $password, 1)){"[+] Credentials Validation: Success"}
   else{"[-] Credentials Validation: Failed"}
 }
